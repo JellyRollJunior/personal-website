29ef50cbbfb6e7af56d4ebc53a9c648cc247961b
Brandon Lin <brandonlin9716@gmail.com>
2025-11-07 21:50:14 +0800

    Create moving down fade in / fade out animation for ArrowDownLink

--- client/src/components/ArrowDownLink.jsx
+++ client/src/components/ArrowDownLink.jsx
@@ -4,8 +4,25 @@ import { motion } from 'motion/react';
 
 const ArrowDownLink = ({ className, href = '' }) => {
   return (
-    <a className={`${className}`} href={href}>
-      <img src={arrowDown} />
+    <a className={`${className} relative size-10`} href={href}>
+      {[...Array(3)].map((item, index) => (
+        <motion.img
+          src={arrowDown}
+          className="absolute top-0 left-1/2 w-full -translate-x-1/2"
+          animate={{
+            opacity: [0, 1, 0], // fade in -> visible -> fade out
+            translateY: [-40, 0, 0],
+          }}
+          transition={{
+            duration: 1.5,
+            ease: [0.25, 0.1, 0.25, 1],
+            times: [0, 0.6, 1],
+            repeat: Infinity,
+            repeatDelay: 1,
+            delay: 0 + index * 0.25,
+          }}
+        />
+      ))}
     </a>
   );
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <h1 className="text-8xl font-medium">Brandon Lin</h1>
         <h3 className="mt-2 text-end text-2xl">FULL STACK DEVELOPER</h3>
       </div>
-      <ArrowDownLink className="mt-auto mb-4 justify-self-end" />
+      <ArrowDownLink className="mt-auto mb-3 justify-self-end" />
     </header>
   );
 };
