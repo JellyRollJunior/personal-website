90d0fb3b460f11ad09fe228a56e7ea6f7fa8ce49
Brandon Lin <brandonlin9716@gmail.com>
2025-11-07 18:49:57 +0800

    Redirect all routes to index

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,11 +1,16 @@
 import { App } from './App.jsx';
 import { Index } from './pages/index.jsx';
+import { Navigate } from 'react-router-dom';
 
 const routes = [
   {
     path: '/',
     element: <App />,
     children: [{ index: '/', element: <Index /> }],
+  },
+  {
+    path: '/*',
+    element: <Navigate to="/" />,
   },
 ];
 
