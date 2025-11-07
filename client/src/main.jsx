5b386a473d3686a33179a62cd5db0b679248d89b
Brandon Lin <brandonlin9716@gmail.com>
2025-11-07 18:34:58 +0800

    Install prettier tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Hello World!</h1>
-  </StrictMode>,
-)
+    <h1 className="text-3xl font-bold">Hello World!</h1>
+  </StrictMode>
+);
