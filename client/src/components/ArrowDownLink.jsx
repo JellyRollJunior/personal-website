66c71a2a6b2f20fd28490e9b9575fe3eaa4c85b4
Brandon Lin <brandonlin9716@gmail.com>
2025-11-09 21:10:15 +0800

    Move name to header center. Fix: ArrowDownLink not showing on screen

--- client/src/components/ArrowDownLink.jsx
+++ client/src/components/ArrowDownLink.jsx
@@ -2,29 +2,40 @@ import arrowDown from '../assets/icons/arrow-down.svg';
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'motion/react';
 
-const ArrowDownLink = ({ className, href = '' }) => {
+const ArrowDownLink = ({ className = '', href = '', size = 40 }) => {
   return (
-    <a className={`${className} relative size-10`} href={href}>
-      {[...Array(3)].map((item, index) => (
-        <motion.img
-          key={index}
-          src={arrowDown}
-          className="absolute top-0 left-1/2 w-full -translate-x-1/2"
-          animate={{
-            opacity: [0, 1, 0], // fade in -> visible -> fade out
-            translateY: [-40, 0, 0],
-          }}
-          transition={{
-            duration: 1.5,
-            ease: [0.25, 0.1, 0.25, 1],
-            times: [0, 0.6, 1],
-            repeat: Infinity,
-            repeatDelay: 1,
-            delay: 0 + index * 0.25,
-          }}
-        />
-      ))}
-    </a>
+    <div
+      className={`relative ${className}`}
+      style={{
+        width: size,
+        height: size,
+      }}
+    >
+      <a className={`size-full`} href={href}>
+        {[...Array(3)].map((item, index) => (
+          <motion.img
+            key={index}
+            src={arrowDown}
+            className="absolute top-0 left-1/2 -translate-x-1/2"
+            style={{
+              width: size,
+            }}
+            animate={{
+              opacity: [0, 1, 0], // fade in -> visible -> fade out
+              translateY: [-40, 0, 0],
+            }}
+            transition={{
+              duration: 1.5,
+              ease: [0.25, 0.1, 0.25, 1],
+              times: [0, 0.6, 1],
+              repeat: Infinity,
+              repeatDelay: 1,
+              delay: 0 + index * 0.25,
+            }}
+          />
+        ))}
+      </a>
+    </div>
   );
 };
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,34 +1,21 @@
 import { HeaderBackground } from './HeaderHudBackground.jsx';
-// eslint-disable-next-line no-unused-vars
-import { easeOut, motion } from 'motion/react';
+import { ArrowDownLink } from './ArrowDownLink.jsx';
 
 const Header = () => {
   return (
     <header className="h-screen p-5">
       <div className="border-yellow relative grid h-full grid-cols-3 grid-rows-3 flex-col overflow-clip rounded-md border-2">
-        <h1 className="text-blue text-shadow-wrap col-span-3 mt-13 flex justify-center text-center text-3xl lg:text-6xl/17">
-          B R A N D O N <br /> L I N
-        </h1>
-        <div></div>
-        <div className="flex items-center justify-center">
-          <motion.a
-            href=""
-            className="border-yellow text-yellow h-fit rounded-lg border-2 px-4 pt-1 pb-2"
-            whileHover={{
-              background: '#ffcc77',
-              color: '#000000',
-              transition: { duration: 1, ease: easeOut },
-            }}
-          >
-            LAUNCH
-          </motion.a>
+        <div className="col-span-3"></div>
+        <div className="text-blue col-span-3 flex items-center justify-center">
+          <h1 className="border-yellow mt-4 rounded-lg border bg-black/40 px-3 pb-3 text-center text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] lg:mt-2 lg:px-5 lg:pb-5 lg:text-6xl/17">
+            B R A N D O N <br className="lg:hidden" />{' '}
+            <span className="hidden lg:inline-block">‣</span> L I N
+          </h1>
         </div>
-        <div></div>
-        <h2 className="text-blue col-span-3 mb-10 flex items-end justify-center text-center text-2xl lg:mb-13 lg:text-4xl/17">
-          <a href="" className="">
-            FULL STACK DEVELOPER
-          </a>
-        </h2>
+        <div />
+        <div className="flex items-end justify-center">
+          <ArrowDownLink size={50} />
+        </div>
         {/* Background Decoration */}
         <HeaderBackground />
       </div>
