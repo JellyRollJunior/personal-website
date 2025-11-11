c22900cd267243e2c5440319b8fd0596d21587c4
Brandon Lin <brandonlin9716@gmail.com>
2025-11-11 19:46:28 +0800

    Create image grid layout for image carousel

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,13 +1,43 @@
+import { useState } from 'react';
+import image from '../assets/icons/arrow-down.svg';
+
+const ImageCarousel = ({ className, imageArray }) => {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  return (
+    <div className={`isolate ${className}`}>
+      <div className="grid grid-cols-9">
+        <img
+          className="border-yellow bg-red z-0 col-span-7 col-start-1 row-span-7 row-start-1 aspect-16/10 w-full border-2"
+          src={imageArray[imgIndex % imageArray.length]}
+        />
+        <img
+          className="border-yellow bg-blue z-1 col-span-7 col-start-2 row-span-7 row-start-2 aspect-16/10 w-full border-2"
+          src={imageArray[(imgIndex + 1) % imageArray.length]}
+        />
+        <img
+          className="border-yellow bg-green z-0 col-span-7 col-start-3 row-span-7 row-start-3 aspect-16/10 w-full border-2"
+          src={imageArray[(imgIndex + 2) % imageArray.length]}
+        />
+      </div>
+      <div className='flex w-full justify-between px-5 mt-3'>
+        <button>prev</button>
+        <button>next</button>
+      </div>
+    </div>
+  );
+};
+
 const Projects = () => {
+  const images = [image, image, image];
+
   return (
     <>
       {/* temp hscreen for working purposes */}
       <section className="min-h-screen px-5">
-        <h2 className="text-blue text-3xl font-extrabold">
-          P R O J E C T S
-        </h2>
+        <h2 className="text-blue text-3xl font-extrabold">P R O J E C T S</h2>
         {/* Chiikawa News Network */}
-        <div className="grid grid-cols-3 mx-8 mt-8">
+        <div className="mx-8 mt-8 grid grid-cols-3 gap-8">
           <div className="self-center justify-self-center">
             <h3 className="text-red text-xl font-bold">
               Chiikawa News Network
@@ -17,18 +47,20 @@ const Projects = () => {
               real time, share posts, customize your profile, and interact
               through likes & comments!
             </p>
-            <div className="flex justify-around mt-5">
-              <button className="border-yellow border rounded-md px-5 py-1">
+            <div className="mt-5 flex justify-around">
+              <button className="border-yellow rounded-md border px-5 py-1">
                 GitHub
               </button>
-              <button className="border-yellow border rounded-md px-5 py-1">
+              <button className="border-yellow rounded-md border px-5 py-1">
                 Live Demo
               </button>
             </div>
           </div>
           <div className="col-span-2 flex items-center justify-center">
-            <div className="w-100  aspect-16/10 border-yellow rounded-md border">
-            </div>
+            <ImageCarousel
+            className='w-1/2'
+              imageArray={images}
+            />
           </div>
         </div>
       </section>
