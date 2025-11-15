cc8afdfa2c919af41bca3059eb6f400579ad691a
Brandon Lin <brandonlin9716@gmail.com>
2025-11-15 11:55:50 +0800

    Make project content layout horizontal

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -40,26 +40,26 @@ const Project = ({
   carouselReverseOrientation = false,
 }) => {
   return (
-    <div className={`rounded-sm ${className}`}>
-      <div className="flex items-center justify-center">
+    <div className={`grid grid-cols-7 ${className} `}>
+      <div className="col-span-4 flex items-center justify-center">
         <ImageCarousel
           imageArray={carouselImagesArray}
           reverseOrientation={carouselReverseOrientation}
         />
       </div>
-      <div className="mt-7 w-full self-center justify-self-center">
+      <div className="col-span-2 flex w-full flex-col justify-center">
         <h3 className="text-red text-center text-2xl font-bold">{title}</h3>
-        <div className="mt-2 flex flex-wrap justify-center gap-x-2 px-14">
-          {techStackArray.map((tech) => (
-            <div className="grid grid-cols-[16px_1fr] items-center gap-2 rounded-md px-3 py-1">
-              <img className="w-full" src={tech.icon} />
-              <div className="mt-1 text-xs">{tech.name}</div>
-            </div>
-          ))}
-        </div>
         <p className="mx-auto mt-3.5 max-w-md text-center">{description}</p>
         {/* Link buttons github an live demo */}
       </div>
+      <ul className="flex flex-col justify-center">
+        {techStackArray.map((tech) => (
+          <li className="grid grid-cols-[16px_1fr] items-center gap-2 rounded-md px-3 py-1">
+            <img className="w-full" src={tech.icon} />
+            <div className="mt-1 text-xs">{tech.name}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
@@ -112,50 +112,44 @@ const Projects = () => {
           </h2>
           <div className="yellow-stripes flex-1 -scale-x-100" />
         </div>
-        <div className="mt-8 grid grid-cols-2 items-center">
+        <div className='mt-8 flex flex-col gap-8'>
           <Project
-            className="col-span-2 mx-auto max-w-5xl"
             title="Chiikawa News Network"
             description="Social networking by and for Chiikawa enjoyers. Message friends
-                    in real time through private and public chats, share posts, customize 
-                    your profile, and interact through likes & comments!"
+                      in real time through private and public chats, share posts, customize
+                      your profile, and interact through likes & comments!"
             techStackArray={cnnTechStack}
             carouselImagesArray={cnnImages}
           />
-        </div>
-        <div className="mt-20 grid grid-cols-2 gap-8 px-8">
-          <div className="flex flex-col gap-20">
-            <Project
-              title="Chiikawa I Spy"
-              description="Play a rousing game of I Spy with Chiikawa and friends! Can you
-                      spot all the cute critters?"
-              techStackArray={cisTechStack}
-              carouselImagesArray={cisImages}
-              carouselReverseOrientation={true}
-            />
-            <Project
-              title="Foods of Taiwan"
-              description="A user generated database of Taiwanese foods! Create/edit/delete 
-                      foods & food categories with all entries stored on a remote SQL database"
-              techStackArray={fotTechStack}
-              carouselImagesArray={fotImages}
-            />
-          </div>
-          <div className="flex flex-col gap-20">
-            <Project
-              title="Sailor Moon Memory"
-              description="Play a game of memory with sailor moon powered by Giphy!"
-              techStackArray={smmTechStack}
-              carouselImagesArray={smmImages}
-            />
-            <Project
-              title="Socket Lovers"
-              description="Real time messaging app with a clean minimal design. Message friends 
-                      in real time through private and public chats, and customize your profile!"
-              techStackArray={slTechStack}
-              carouselImagesArray={slImages}
-            />
-          </div>
+          <Project
+          className='w-4/5 self-end'
+            title="Chiikawa I Spy"
+            description="Play a rousing game of I Spy with Chiikawa and friends! Can you
+                        spot all the cute critters?"
+            techStackArray={cisTechStack}
+            carouselImagesArray={cisImages}
+            carouselReverseOrientation={true}
+          />
+          <Project
+            title="Foods of Taiwan"
+            description="A user generated database of Taiwanese foods! Create/edit/delete
+                        foods & food categories with all entries stored on a remote SQL database"
+            techStackArray={fotTechStack}
+            carouselImagesArray={fotImages}
+          />
+          <Project
+            title="Sailor Moon Memory"
+            description="Play a game of memory with sailor moon powered by Giphy!"
+            techStackArray={smmTechStack}
+            carouselImagesArray={smmImages}
+          />
+          <Project
+            title="Socket Lovers"
+            description="Real time messaging app with a clean minimal design. Message friends
+                        in real time through private and public chats, and customize your profile!"
+            techStackArray={slTechStack}
+            carouselImagesArray={slImages}
+          />
         </div>
       </section>
     </>
