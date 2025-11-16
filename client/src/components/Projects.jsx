727fb9215c934c4a588b20e8f0cf0c7664146d7b
Brandon Lin <brandonlin9716@gmail.com>
2025-11-16 09:38:30 +0800

    Separate projects. Create tab container for other projects

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,3 +1,4 @@
+import { useState, Fragment } from 'react';
 import { ImageCarousel } from './ImageCarousel.jsx';
 /* Chiikawa News Network */
 import cnnChat from '../assets/projects/chiikawaNewsNetwork/chat.webp';
@@ -28,6 +29,69 @@ import jest from '../assets/icons/jest.svg';
 import pg from '../assets/icons/postgresql.svg';
 import js from '../assets/icons/js.svg';
 import html from '../assets/icons/html.svg';
+// eslint-disable-next-line no-unused-vars
+import { motion, AnimatePresence } from 'motion/react';
+
+/* Project Data */
+const cnnImages = [cnnChat, cnnHome, cnnMobile, cnnProfile];
+const cnnTechStack = [
+  { icon: react, name: 'React' },
+  { icon: nodejs, name: 'NodeJS' },
+  { icon: express, name: 'Express' },
+  { icon: prisma, name: 'PrismaORM' },
+  { icon: socketio, name: 'Socket.IO' },
+];
+const cisImages = [cisGameplay, cisStart, cisWin];
+const cisTechStack = [
+  { icon: react, name: 'React' },
+  { icon: nodejs, name: 'NodeJS' },
+  { icon: express, name: 'Express' },
+  { icon: prisma, name: 'PrismaORM' },
+  { icon: socketio, name: 'Socket.IO' },
+  { icon: jest, name: 'Jest' },
+];
+const fotImages = [fotHome, fotCategories, fotEdit];
+const fotTechStack = [
+  { icon: js, name: 'Javascript' },
+  { icon: html, name: 'HTML' },
+  { icon: js, name: 'EJS' },
+  { icon: nodejs, name: 'NodeJS' },
+  { icon: express, name: 'Express' },
+  { icon: pg, name: 'PostgreSQL' },
+];
+const slImages = [slProfile, slHome, slChat];
+const slTechStack = [
+  { icon: react, name: 'React' },
+  { icon: nodejs, name: 'NodeJS' },
+  { icon: express, name: 'Express' },
+  { icon: prisma, name: 'PrismaORM' },
+  { icon: socketio, name: 'Socket.IO' },
+];
+const smmImages = [smmHome];
+const smmTechStack = [{ icon: react, name: 'React' }];
+/* Project Lists */
+const otherProjectsList = [
+  {
+    title: 'Foods of Taiwan',
+    description:
+      'A user generated database of Taiwanese foods! Create/edit/delete foods & food categories with all entries stored on a remote SQL database',
+    techStackArray: fotTechStack,
+    carouselImageArray: fotImages,
+  },
+  {
+    title: 'Sailor Moon Memory',
+    description: 'Play a game of memory with sailor moon powered by Giphy!',
+    techStackArray: smmTechStack,
+    carouselImageArray: smmImages,
+  },
+  {
+    title: 'Socket Lovers',
+    description:
+      'Real time messaging app with a clean minimal design. Message friends in real time through private and public chats, and customize your profile!',
+    techStackArray: slTechStack,
+    carouselImageArray: slImages,
+  },
+];
 
 const Project = ({
   className,
@@ -36,23 +100,29 @@ const Project = ({
   description,
   // githubLink,
   // demoLink,
-  carouselImagesArray,
-  carouselReverseOrientation = false,
+  carouselImageArray,
+  reverseOrientation = false,
 }) => {
   return (
     <div className={`grid grid-cols-7 ${className} `}>
-      <div className="col-span-4 flex items-center justify-center">
+      <div
+        className={`col-span-4 flex items-center justify-center ${reverseOrientation && 'col-start-4'}`}
+      >
         <ImageCarousel
-          imageArray={carouselImagesArray}
-          reverseOrientation={carouselReverseOrientation}
+          imageArray={carouselImageArray}
+          reverseOrientation={reverseOrientation}
         />
       </div>
-      <div className="col-span-2 flex w-full flex-col justify-center">
+      <div
+        className={`col-span-2 flex w-full flex-col justify-center ${reverseOrientation && 'col-start-2 row-start-1'}`}
+      >
         <h3 className="text-red text-center text-2xl font-bold">{title}</h3>
         <p className="mx-auto mt-3.5 max-w-md text-center">{description}</p>
         {/* Link buttons github an live demo */}
       </div>
-      <ul className="flex flex-col justify-center">
+      <ul
+        className={`flex flex-col justify-center ${reverseOrientation && 'col-start-1 row-start-1 '}`}
+      >
         {techStackArray.map((tech) => (
           <li className="grid grid-cols-[16px_1fr] items-center gap-2 rounded-md px-3 py-1">
             <img className="w-full" src={tech.icon} />
@@ -64,94 +134,86 @@ const Project = ({
   );
 };
 
-const Projects = () => {
-  const cnnImages = [cnnChat, cnnHome, cnnMobile, cnnProfile];
-  const cnnTechStack = [
-    { icon: react, name: 'React' },
-    { icon: nodejs, name: 'NodeJS' },
-    { icon: express, name: 'Express' },
-    { icon: prisma, name: 'PrismaORM' },
-    { icon: socketio, name: 'Socket.IO' },
-  ];
-  const cisImages = [cisGameplay, cisStart, cisWin];
-  const cisTechStack = [
-    { icon: react, name: 'React' },
-    { icon: nodejs, name: 'NodeJS' },
-    { icon: express, name: 'Express' },
-    { icon: prisma, name: 'PrismaORM' },
-    { icon: socketio, name: 'Socket.IO' },
-    { icon: jest, name: 'Jest' },
-  ];
-  const fotImages = [fotHome, fotCategories, fotEdit];
-  const fotTechStack = [
-    { icon: js, name: 'Javascript' },
-    { icon: html, name: 'HTML' },
-    { icon: js, name: 'EJS' },
-    { icon: nodejs, name: 'NodeJS' },
-    { icon: express, name: 'Express' },
-    { icon: pg, name: 'PostgreSQL' },
-  ];
-  const slImages = [slProfile, slHome, slChat];
-  const slTechStack = [
-    { icon: react, name: 'React' },
-    { icon: nodejs, name: 'NodeJS' },
-    { icon: express, name: 'Express' },
-    { icon: prisma, name: 'PrismaORM' },
-    { icon: socketio, name: 'Socket.IO' },
-  ];
-  const smmImages = [smmHome];
-  const smmTechStack = [{ icon: react, name: 'React' }];
-
+const FeaturedProjects = () => {
   return (
-    <>
-      <section className="border-yellow rounded-sm rounded-b-3xl border-2 pb-14">
-        <div className="border-yellow flex border-b-2">
-          <div className="yellow-stripes flex-1" />
-          <h2 className="text-blue border-yellow border-x-2 px-5 pt-2 pb-1 text-center text-3xl font-extrabold">
-            P R O J E C T S
-          </h2>
-          <div className="yellow-stripes flex-1 -scale-x-100" />
-        </div>
-        <div className='mt-8 flex flex-col gap-8'>
-          <Project
-            title="Chiikawa News Network"
-            description="Social networking by and for Chiikawa enjoyers. Message friends
+    <section>
+      <h2 className="text-blue text-center text-3xl font-extrabold">
+        P R O J E C T S
+      </h2>
+      <div className="mt-8 flex flex-col gap-8">
+        <Project
+          title="Chiikawa News Network"
+          description="Social networking by and for Chiikawa enjoyers. Message friends
                       in real time through private and public chats, share posts, customize
                       your profile, and interact through likes & comments!"
-            techStackArray={cnnTechStack}
-            carouselImagesArray={cnnImages}
-          />
-          <Project
-          className='w-4/5 self-end'
-            title="Chiikawa I Spy"
-            description="Play a rousing game of I Spy with Chiikawa and friends! Can you
+          techStackArray={cnnTechStack}
+          carouselImageArray={cnnImages}
+        />
+        <Project
+          className="mr-20 w-4/5 self-end"
+          title="Chiikawa I Spy"
+          description="Play a rousing game of I Spy with Chiikawa and friends! Can you
                         spot all the cute critters?"
-            techStackArray={cisTechStack}
-            carouselImagesArray={cisImages}
-            carouselReverseOrientation={true}
-          />
-          <Project
-            title="Foods of Taiwan"
-            description="A user generated database of Taiwanese foods! Create/edit/delete
-                        foods & food categories with all entries stored on a remote SQL database"
-            techStackArray={fotTechStack}
-            carouselImagesArray={fotImages}
-          />
-          <Project
-            title="Sailor Moon Memory"
-            description="Play a game of memory with sailor moon powered by Giphy!"
-            techStackArray={smmTechStack}
-            carouselImagesArray={smmImages}
-          />
-          <Project
-            title="Socket Lovers"
-            description="Real time messaging app with a clean minimal design. Message friends
-                        in real time through private and public chats, and customize your profile!"
-            techStackArray={slTechStack}
-            carouselImagesArray={slImages}
-          />
-        </div>
-      </section>
+          techStackArray={cisTechStack}
+          carouselImageArray={cisImages}
+          reverseOrientation={true}
+        />
+      </div>
+    </section>
+  );
+};
+
+const OtherProjects = () => {
+  const [selectedProject, setSelectedProject] = useState(
+    otherProjectsList[0].title
+  );
+
+  return (
+    <section className="border-yellow rounded-md border-2">
+      <h2 className="text-blue mt-8 text-center text-3xl font-extrabold">
+        O T H E R ‣ W O R K S
+      </h2>
+      <ul className="grid grid-cols-3">
+        {otherProjectsList.map((project) => (
+          <li key={project.title}>
+            <button
+              className="size-full"
+              onClick={() => setSelectedProject(project.title)}
+            >
+              {project.title}
+              {project.title === selectedProject && (
+                <motion.div
+                  className="bg-blue h-1"
+                  layoutId="underline"
+                  id="underline"
+                />
+              )}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="mt-8 px-5">
+        {otherProjectsList.map((project) => (
+          <Fragment key={project.title}>
+            <Project
+              className={`${selectedProject !== project.title && 'hidden'}`}
+              title={project.title}
+              description={project.description}
+              techStackArray={project.techStackArray}
+              carouselImageArray={project.carouselImageArray}
+            />
+          </Fragment>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+const Projects = () => {
+  return (
+    <>
+      <FeaturedProjects />
+      <OtherProjects />
     </>
   );
 };
