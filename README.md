46270ef450ad76ba5410d249a3123ec4c0cad333
Brandon Lin <brandonlin9716@gmail.com>
2025-11-19 21:04:56 +0800

    Change grid-cols depending on screen size. Stagger tech item fade in

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My personal website. Includes my works, profile, and interests
 
 #### TODO
 
--   Skills & tools
+- languages
--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,25 +1,43 @@
 import { FRONTEND, BACKEND, MISCELLANEOUS } from '../data/skills.js';
+// eslint-disable-next-line no-unused-vars
+import { motion, stagger } from 'motion/react';
 
 const SkillsCard = ({ className, title, techStackArray }) => {
   if (!techStackArray || !Array.isArray(techStackArray) || !title) return null;
   return (
     <div className={`w-full ${className}`}>
       <h3 className="text-red text-center text-xl">{title}</h3>
-      <ul className="mt-5 grid grid-cols-3 gap-x-3 gap-y-7">
+      <motion.ul
+        className="mt-5 grid grid-cols-3 gap-x-3 gap-y-7"
+        variants={{
+          hidden: {},
+          fadeIn: {
+            transition: {
+              delayChildren: stagger(0.1),
+            },
+          },
+        }}
+        initial="hidden"
+        whileInView="fadeIn"
+        viewport={{
+          once: true,
+          amount: 0.5,
+        }}
+      >
         {techStackArray.map((pair) => (
-          <li
+          <motion.li
             key={pair.name}
             className="flex flex-col items-center gap-1"
             variants={{
-              hidden: { opacity: 0, x: 15 },
-              visible: { opacity: 1, x: 0 },
+              hidden: { opacity: 0, x: -20 },
+              fadeIn: { opacity: 1, x: 0 },
             }}
           >
             <img className="size-[32px]" src={pair.icon} />
             <div className="text-center text-sm break-keep">{pair.name}</div>
-          </li>
+          </motion.li>
         ))}
-      </ul>
+      </motion.ul>
     </div>
   );
 };
@@ -30,7 +48,7 @@ const Skills = () => {
       <h2 className="text-blue text-center text-3xl font-extrabold">
         S K I L L S
       </h2>
-      <div className="mx-auto mt-5 grid max-w-[900px] grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
+      <div className="mx-auto mt-5 grid max-w-[1100px] grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
         <SkillsCard
           className="mx-auto max-w-[380px]"
           title={'Frontend'}
