61e0df36f89ca8b8c31b3cb90aebe84882718e56
Brandon Lin <brandonlin9716@gmail.com>
2025-11-19 17:00:16 +0800

    Add postman, supabase, tailwind icons. Organize tech into sections

---
+++ client/src/data/skills.js
@@ -0,0 +1,32 @@
+import * as tech from '../data/tech.js';
+
+const FRONTEND = [
+    tech.react,
+    tech.javascript,
+    tech.html,
+    tech.ejs,
+    tech.css,
+    tech.jest,
+    tech.motion,
+    tech.tailwind,
+];
+
+const BACKEND = [
+    tech.nodeJs,
+    tech.express,
+    tech.socketIo,
+    tech.jest,
+    tech.postgreSql,
+    tech.prisma,
+];
+
+const MISCELLANEOUS = [
+    tech.git,
+    tech.github,
+    tech.postman,
+    tech.linux,
+    tech.vscode,
+    tech.supabase,
+];
+
+export { FRONTEND, BACKEND, MISCELLANEOUS };
--- client/src/data/tech.js
+++ client/src/data/tech.js
@@ -8,17 +8,23 @@ import pgIcon from '../assets/icons/postgresql.svg';
 import jsIcon from '../assets/icons/js.svg';
 import htmlIcon from '../assets/icons/html.svg';
 import ejsIcon from '../assets/icons/ejs.svg';
+import cssIcon from '../assets/icons/css.svg';
 import gitIcon from '../assets/icons/git.svg';
 import githubIcon from '../assets/icons/github.svg';
 import linuxIcon from '../assets/icons/linux.svg';
 import motionIcon from '../assets/icons/motion.svg';
 import vscodeIcon from '../assets/icons/vscode.svg';
+import postmanIcon from '../assets/icons/postman.svg';
+import tailwindIcon from '../assets/icons/tailwind.svg';
+import supabaseIcon from '../assets/icons/supabase.svg';
 
 /* Frontend */
 const react = { icon: reactIcon, name: 'React' };
 const javascript = { icon: jsIcon, name: 'Javascript' };
 const html = { icon: htmlIcon, name: 'HTML' };
 const ejs = { icon: ejsIcon, name: 'EJS' };
+const css = { icon: cssIcon, name: 'CSS' };
+const tailwind = { icon: tailwindIcon, name: 'TailwindCSS' };
 /* Backend */
 const nodeJs = { icon: nodejsIcon, name: 'NodeJS' };
 const express = { icon: expressIcon, name: 'Express' };
@@ -27,26 +33,35 @@ const socketIo = { icon: socketioIcon, name: 'Socket.IO' };
 const jest = { icon: jestIcon, name: 'Jest' };
 const postgreSql = { icon: pgIcon, name: 'PostgreSQL' };
 /* Tools */
-const git = { icon: gitIcon, name: 'Git' }
-const github = { icon: githubIcon, name: 'Github' }
-const linux  = { icon: linuxIcon , name: 'Linux'}
-const motion  = { icon: motionIcon, name: 'Framer Motion'}
-const vscode = { icon: vscodeIcon, name: 'VS Code'}
+const git = { icon: gitIcon, name: 'Git' };
+const github = { icon: githubIcon, name: 'Github' };
+const linux = { icon: linuxIcon, name: 'Linux' };
+const motion = { icon: motionIcon, name: 'Framer Motion' };
+const vscode = { icon: vscodeIcon, name: 'VS Code' };
+const postman = { icon: postmanIcon, name: 'Postman' };
+const supabase = { icon: supabaseIcon, name: 'Supabase' };
 
 export {
+    // Frontend
     react,
     javascript,
     html,
     ejs,
+    css,
+    tailwind,
+    // Backend
     nodeJs,
     express,
     prisma,
     socketIo,
     jest,
     postgreSql,
+    // Tools
     git,
     github,
     linux,
     motion,
     vscode,
+    postman,
+    supabase,
 };
